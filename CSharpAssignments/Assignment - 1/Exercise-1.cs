2d2d3c6e5495b1395740025f896535696e0145a0
Bindu Madhavi <bindumadhaviamirineni@gmail.com>
2026-03-16 12:21:53 +0530

    Class Assignment

---
+++ CSharpAssignments/Assignment - 1/Exercise-1.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace C__Basic_Assignments1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Exercise_1 obj = new Exercise_1();
+            obj.Divided();
+
+            Console.ReadLine();
+        }
+    }
+}
+    internal class Exercise_1
+    {
+        public void Divided()
+        {
+            Console.WriteLine("Enter First value:");
+            int a = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter Second value:");
+            int b = Convert.ToInt32(Console.ReadLine());
+            if (b != 0)
+            {
+                Console.WriteLine("Quotient: " + (a / b));
+            }
+            else
+            {
+                Console.WriteLine("Second number should not be zero");
+            }
+        }
+    }
---
+++ CSharpAssignments/ClassAssignment/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace ClassAssignment
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
